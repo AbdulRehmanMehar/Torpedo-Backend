a8f4fa4510329b3bb8d0de35673cc66ae397cce5
Abdul Rehman <mehars.6925@gmail.com>
2023-02-11 20:03:56 +0500

    completed the products controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,8 @@
-import { Response } from 'express';
-import { v4 as uuidV4 } from 'uuid';
-import { Invoice, Payment, Product } from '../models';
-import { Body, Controller, Get, Post, Req, Res, UseBefore } from 'routing-controllers';
+import { Response, Request } from 'express';
 import { productSchema } from '../validators';
+import { connectToDatabase } from '../config/db';
 import { Auth0Middleware } from '../middlewares/auth0.middleware';
+import { Body, Controller, Delete, Get, Post, Put, QueryParam, Req, Res, UseBefore } from 'routing-controllers';
 
 @UseBefore(Auth0Middleware)
 @Controller('/products')
@@ -12,6 +11,8 @@ export class ProductsController {
   @Get('/')
   async getProducts(@Req() request: any, @Res() response: Response) {
     try {
+      const { userId, encKey, tenantId } = request.auth.currentUser.user_metadata;
+      const { Product } = await connectToDatabase(tenantId);
       const products = await Product.findAll({
         attributes: ['id', 'name', 'price', 'width', 'height']
       });
@@ -28,17 +29,9 @@ export class ProductsController {
   }
 
   @Post('/add')
-  async addProduct(@Body() productData: any, @Res() response: Response) {
+  async addProduct(@Body() productData: any, @Req() request: any, @Res() response: Response) {
     try {
-
-      console.log({ productData });
-
-
       const { value, error } = productSchema.validate(productData);
-
-      console.log({ value });
-
-
       if (error) return response.status(400).json({ error });
 
       const {
@@ -47,6 +40,9 @@ export class ProductsController {
         width,
         height,
       } = value;
+
+      const { userId, encKey, tenantId } = request.auth.currentUser.user_metadata;
+      const { Product } = await connectToDatabase(tenantId);
 
       const product = await Product.create({
         name,
@@ -62,6 +58,70 @@ export class ProductsController {
     } catch (error) {
       console.log(error);
 
+      return response.status(500).json({
+        message: 'Something went wrong',
+        error
+      });
+    }
+  }
+
+  @Put('/update/:productId')
+  async updateProduct(@QueryParam('productId') productId: string, @Body() productData: any, @Req() request: any, @Res() response: Response) {
+    try {
+      const { value, error } = productSchema.validate(productData);
+      if (error) return response.status(400).json({ error });
+
+      const {
+        name,
+        price,
+        width,
+        height,
+      } = value;
+
+      const { tenantId } = request.auth.currentUser.user_metadata;
+      const { Product } = await connectToDatabase(tenantId);
+
+      const product = await Product.update({
+        name,
+        price,
+        width,
+        height,
+        tenantId: null
+      }, {
+        where: {
+          id: productId
+        }
+      });
+
+      return response.status(200).json({
+        product
+      });
+    } catch (error) {
+      console.log(error);
+
+      return response.status(500).json({
+        message: 'Something went wrong',
+        error
+      });
+    }
+  }
+
+  @Delete('/delete/:productId')
+  async deleteProduct(@QueryParam('productId') productId: string, @Req() request: any, @Res() response: Response) {
+    try {
+      const { userId, encKey, tenantId } = request.auth.currentUser.user_metadata;
+      const { Product } = await connectToDatabase(tenantId);
+      
+      await Product.destroy({
+        where: {
+          id: productId,
+        },
+      });
+
+      return response.json({
+        message: 'Deleted the product'
+      });
+    } catch (error) {
       return response.status(500).json({
         message: 'Something went wrong',
         error
